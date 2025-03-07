51c3a0ccbf0fb51a8c00aad0a9bdd6533b890deb
Rehman Saqib <rehmansaqibawan@gmail.com>
2025-03-07 10:44:22 +0500

    Your commit message here

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # this is the main file which will be used to make some of the calls to api,tools,chains and Agents while using langchain
 import os 
 from dotenv import load_dotenv
-from langchain_groq import chatgroq
+from langchain_groq import ChatGroq
 import yfinance as yf
 from langchain.agents import Agent, tools, initialize_agent
-from langchain.chains import Chain
+import environ
+import certifi
+
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Load the API key
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["GROQ_API_KEY"] = env("GROQ_API_KEY")
+print("GROQ_API_KEY:", os.getenv("GROQ_API_KEY"))
+llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+response = llm.invoke("What is the stock price of Apple?")
+print(response)
